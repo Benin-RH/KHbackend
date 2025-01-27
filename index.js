aa30a216cb52d4571d8bdf1b0d19fddb2180063a
AskinasNatemin <askinas2002@gmail.com>
2025-01-27 21:05:15 +0530

    feat:new files are added

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express=require("express");
-// const cors = require("cors");
-const bodyParser=require("body-parser");
-const routes=require('./routes.js')
-const db=require ("./dbConnection.js")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes.js");
+const db = require("./dbConnection.js");
 
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
-const app=express();
+app.use(cors(corsOptions));
 const port = 5001;
 
-app.use(bodyParser.json())
-app.use('/',routes)
+app.use(bodyParser.json());
+app.use("/", routes);
 
-
-app.listen(port,()=>{
-    console.log(`App is running at http://localhost:${port}`); 
-})
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});

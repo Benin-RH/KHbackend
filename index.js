171edb1c5a63239753ecd9a9749b37446e524c1d
Benin-RH <128675525+Benin-RH@users.noreply.github.com>
2025-01-28 12:00:21 +0530

    New files are added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const bodyParser=require("body-parser");
 const routes=require('./routes.js')
 const db=require ("./dbConnection.js")
@@ -8,6 +8,12 @@ const db=require ("./dbConnection.js")
 const app=express();
 const port = 5001;
 
+//Configure CORS
+const corsOptions ={
+    origin: "http://localhost:3000", //Allow request from react app
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use('/',routes)
 

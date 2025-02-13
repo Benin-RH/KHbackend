238e59cea986712bcec511611410ed59c2cbf007
AskinasNatemin <askinas2002@gmail.com>
2025-02-13 20:41:14 +0530

    feat:new files are added

--- Staff/staffControler.js
+++ Staff/staffControler.js
@@ -1,29 +1,53 @@
-const staffschema = require("./staffschema");
+const staffschema = require('./staffschema');
 
-const staffRegistration = async (req, res) => {
-  const { Name, Email, Password, Contact } = req.body;
+const staffRegistration = (req, res) => {
+  let { Name, Email:email, Password, Contact } = req.body;
 
-  if (!Name || !Email || !Password || !Contact) {
+  let pass = Password.toString().split("").length;  
+  if (!Name || !email || !Password || !Contact) {
     return res.status(400).json({ message: "All fields are required." });
   }
+  staffschema.findOne({ email })
+    .then((existingStaff) => {
 
-  try {
-    const existingStaff = await staffschema.findOne({ Email });
-    if (existingStaff) {
-      return res.status(400).json({ message: "Email is already registered." });
-    }
-    const staff = new staffschema({
-      staffname: Name,
-      email:Email,
-      password:Password,
-      contact: Contact,
+      console.log(existingStaff);
+      if (existingStaff) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+      if (pass < 6) {
+        return res.status(400).json({ message: "Password length must be at least 6" });
+      }
+
+      if (isNaN(Password)) {
+        return res.status(400).json({ message: "Your password should be a number" });
+      }
+
+      const staff = new staffschema({
+        staffname: Name,
+        email,
+        password: Password,
+        contact: Contact,
+      });
+
+      staff.save()
+        .then((response) => {
+          return res.status(200).json({
+            data: response,
+            message: "Registration success",
+          });
+        })
+        .catch((err) => {
+          return res.status(500).json({
+            message: "An error occurred while saving the staff.",
+          });
+        });
+
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        message: "An error occurred while checking the email.",
+      });
     });
-    const response = await staff.save();
-    res.status(201).json({ message: "Registration successful!", data: response });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error. Please try again." });
-  }
 };
 
 module.exports = {
